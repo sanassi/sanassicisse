2726746d43dc158c5b4f9ac90df86f9e8f08e541
Sanassi Cissé <sanassi-mory.cisse@epita.fr>
2023-07-29 01:17:10 +0200

    Updates

--- src/Me.js
+++
@@ -1,9 +0,0 @@
-import './Me.css'
-export default function Me() {
-  return (
-    <div className='Me'>
-      <div className='my-name'>Sanassi Cissé</div>
-      <div className='my-job'>Computer Science Student @ Epita</div>
-    </div>
-  );
-};

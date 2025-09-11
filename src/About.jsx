620f43cc88297407b7adeeb97ed4cd4fcb7ce6cf
Sanassi Cisse <sanassicisse@outlook.fr>
2025-09-11 18:31:49 -0400

    feat: update about section and exp

--- src/About.jsx
+++ src/About.jsx
@@ -1,14 +1,18 @@
-import './About.css'
+import "./About.css";
 export default function About() {
-  let text = ['I am a 22 year old Computer Science Student at Epita Paris.',
-     'I am currently majoring in Embedded and Realtime Systems.',
-     'I enjoy playing / watching basketball and learning to play the electric guitar.'];
+  let text = [
+    "I am a 23 year old Computer Science Student at Epita Paris.",
+    "I am currently majoring in Embedded and Realtime Systems.",
+    "I enjoy playing / watching basketball and learning to play the electric guitar.",
+  ];
   return (
-    <div id='about' className='about'>
-      <label className='about-label'>About</label>
-      <div className='about-text'>
-        {text.map((t) => <div>{t}</div>)}
+    <div id="about" className="about">
+      <label className="about-label">About</label>
+      <div className="about-text">
+        {text.map((t) => (
+          <div>{t}</div>
+        ))}
       </div>
     </div>
   );
-};
+}

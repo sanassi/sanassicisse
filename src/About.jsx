be66769d69555ffa21566995e375105569322594
Sanassi Cisse <sanassicisse@outlook.fr>
2026-03-31 19:49:35 +0200

    feat: update content

--- src/About.jsx
+++ src/About.jsx
@@ -1,9 +1,8 @@
 import "./About.css";
 export default function About() {
   let text = [
-    "I am a 23 year old Computer Science Student at Epita Paris.",
-    "I am currently majoring in Embedded and Realtime Systems.",
-    "I enjoy playing / watching basketball and learning to play the electric guitar.",
+    "I am a 23 year old Computer Science Graduate from Epita Paris.",
+    "I majored in Embedded and Realtime Systems.",
   ];
   return (
     <div id="about" className="about">
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,50 +1,82 @@
 import ProjectInfo from "./ProjectInfo";
 
-import './Projects.css'
+import "./Projects.css";
 export default function Projects() {
   return (
-    <div id='projects'>
-      <label className='project-label'>Fun Projects</label>
+    <div id="projects">
+      <label className="project-label">Fun Projects</label>
       <div className="projects">
-        <ProjectInfo name="Tiger Compiler Backend"
-                     lang={["C++"]}
-                     description={['Compiler for the Tiger Language.', '14 weeks group project.']}
-                     link={{
-                       "site": "other",
-                       "url": "https://assignments.lrde.epita.fr/tools/modern_compiler_implementation/first_editions.html"
-                     }} />
-        <ProjectInfo name="Rash"
-                     lang={["C (C99)"]}
-                     description={['Shell Command Line Interpreter.', 'Conform to the Posix Standard.']}
-                     link={{
-                       "site":"gitlab",
-                       "url":"https://gitlab.com/sanassi/rash"}} />
-        { /* <ProjectInfo name="Red Panda"
+        <ProjectInfo
+          name="Infinite Canvas"
+          lang={["C#", "TypeScript", "React", "Pgsql"]}
+          description={["Collaborative Infinite Canvas App"]}
+          link={{
+            site: "other",
+            url: "https://infinite-canvas.sanassicisse.workers.dev/",
+          }}
+        />
+        <ProjectInfo
+          name="Tiger Compiler Backend"
+          lang={["C++"]}
+          description={[
+            "Compiler for the Tiger Language.",
+            "14 weeks group project.",
+          ]}
+          link={{
+            site: "other",
+            url: "https://assignments.lrde.epita.fr/tools/modern_compiler_implementation/first_editions.html",
+          }}
+        />
+        <ProjectInfo
+          name="Rash"
+          lang={["C (C99)"]}
+          description={[
+            "Shell Command Line Interpreter.",
+            "Conform to the Posix Standard.",
+          ]}
+          link={{
+            site: "gitlab",
+            url: "https://gitlab.com/sanassi/rash",
+          }}
+        />
+        {/* <ProjectInfo name="Red Panda"
                      lang={["Java", "JavaFX"]}
                      description={['IDE for beginners',
                        'With features such as: Autocomplete, Syntax Highlighting.']}
                      link={{
                        "site":"github",
                        "url":"https://github.com/sanassi/red-panda"}} /> */}
-        <ProjectInfo name="PixElArt"
-                     lang={["Javascript", "React"]}
-                     description={['Pixel Art Editor', 'Built with ReactJS']}
-                     link={{
-                       "site":"github",
-                       "url":"https://github.com/sanassi/pixelart-react"}} />
-        <ProjectInfo name="Portfolio Website"
-                     lang={["ReactJS"]}
-                     description={["First iteration of my portfolio website (this!)"]}
-                     link={{
-                       "site":"github",
-                       "url":"https://github.com/sanassi/sanassicisse"}} />
-         <ProjectInfo name="Punto"
-                      lang={["ReactJS", "Socket.IO", "ExpressJS"]}
-                      description={["Punto Board Game", "Multiplayer handled with Socket.IO (2 to 4 players)"]}
-                      link={{
-                         "site":"github",
-                         "url":"https://github.com/sanassi/punto"}} />
+        <ProjectInfo
+          name="PixElArt"
+          lang={["Javascript", "React"]}
+          description={["Pixel Art Editor", "Built with ReactJS"]}
+          link={{
+            site: "github",
+            url: "https://github.com/sanassi/pixelart-react",
+          }}
+        />
+        <ProjectInfo
+          name="Portfolio Website"
+          lang={["ReactJS"]}
+          description={["First iteration of my portfolio website (this!)"]}
+          link={{
+            site: "github",
+            url: "https://github.com/sanassi/sanassicisse",
+          }}
+        />
+        <ProjectInfo
+          name="Punto"
+          lang={["ReactJS", "Socket.IO", "ExpressJS"]}
+          description={[
+            "Punto Board Game",
+            "Multiplayer handled with Socket.IO (2 to 4 players)",
+          ]}
+          link={{
+            site: "github",
+            url: "https://github.com/sanassi/punto",
+          }}
+        />
       </div>
     </div>
   );
-};
+}
